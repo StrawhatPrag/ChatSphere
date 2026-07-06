683a43cbdd7cc708b4c92dbf6cdf5f53c106855b
Pragadheesh Chandramohan <cprag2006@gmail.com>
2026-07-06 17:30:56 +0530

    Implement multi-client message broadcasting

--- src/server/ChatServer.java
+++ src/server/ChatServer.java
@@ -3,11 +3,14 @@ import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
 import util.Constants;
+import java.util.ArrayList;
 
 public class ChatServer {
+    private static final int PORT = Constants.PORT;
+    private static ArrayList<ClientHandler> clients=new ArrayList<>();
     public static void main(String args[]){
         try {
-            ServerSocket serverSocket=new ServerSocket(Constants.PORT);
+            ServerSocket serverSocket=new ServerSocket(PORT);
             
             String message="Chat Server Started\nListening on port ";
             System.out.println("*******************************");
@@ -17,7 +20,8 @@ public class ChatServer {
             while(true){
                 Socket clientSocket=serverSocket.accept();
                 System.out.println("New client connected: "+clientSocket.getInetAddress());
-                ClientHandler clientHandler=new ClientHandler(clientSocket);
+                ClientHandler clientHandler=new ClientHandler(clientSocket,clients);
+                clients.add(clientHandler);
                 clientHandler.start();
             }
 
--- src/server/ClientHandler.java
+++ src/server/ClientHandler.java
@@ -4,16 +4,36 @@ import java.io.IOException;
 import java.io.DataInputStream;
 import java.net.Socket;
 import java.io.DataOutputStream;
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class ClientHandler extends Thread{
     private Socket socket;
+    private ArrayList<ClientHandler> clients;
     private DataInputStream input;
     private DataOutputStream output;
-    private Scanner scanner;
 
-    public ClientHandler(Socket socket){
+    public ClientHandler(Socket socket,ArrayList<ClientHandler> clients){
         this.socket=socket;
+        this.clients=clients;
+    }
+
+    public void sendMessage(String message){
+        try {
+            output.writeUTF(message);
+            output.flush();
+        } catch (IOException e) {
+            // TODO: handle exception
+            System.out.println("Unable to send message.");
+        }
+    }
+
+    private void broadcast(String message){
+        for (ClientHandler client:clients){
+            if (client!=this){
+                client.sendMessage(message);
+            }
+        }
     }
 
     @Override
@@ -23,17 +43,19 @@ public class ClientHandler extends Thread{
             output=new DataOutputStream(socket.getOutputStream());
             ServerSender sender=new ServerSender(output);
             sender.start();
-            scanner=new Scanner(System.in);
             System.out.println("Handler started for "+ socket.getInetAddress());
             while (true) {
                 String message=input.readUTF();
                 System.out.println("Received: "+message);
+                broadcast(message);
             }
-        } catch (Exception e) {
+        } catch (IOException e) {
             // TODO: handle exception
             System.out.println("Connection lost with " + socket.getInetAddress());
         }
         finally {
+            clients.remove(this);
+
             try {
                 socket.close();
             } catch (IOException ignored) {}
